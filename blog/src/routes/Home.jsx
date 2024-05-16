186463c851e2c7b06050873506d03828f5fc15fb
celoaraujo <celoparaujo99@gmail.com>
2024-05-16 17:42:24 -0300

    utilizando base url no axios

--- blog/src/routes/Home.jsx
+++ blog/src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import blogFetch from "../axios/config";
 import { Link } from "react-router-dom";
 
 import "./Home.css";
@@ -9,9 +9,7 @@ const Home = () => {
 
   const getPosts = async () => {
     try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
+      const response = await blogFetch.get("/posts");
       const data = response.data;
       setPosts(data);
     } catch (error) {
@@ -23,17 +21,24 @@ const Home = () => {
     getPosts();
   }, []);
 
-  return <div className="home">
-    <h1>Últimos posts</h1>
-    {posts.length === 0 ? <p>Carregando...</p> : (posts.map((post)=>(
-      <div className="post" key={post.id}>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-        <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
-      </div>
-    ))
-  )}
-  </div>;
+  return (
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
