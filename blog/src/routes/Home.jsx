24e758872e648d72d7c00e91f3d6059375975d2b
celoaraujo <celoparaujo99@gmail.com>
2024-05-16 17:15:15 -0300

    carregando API

--- blog/src/routes/Home.jsx
+++ blog/src/routes/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import "./Home.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default Home
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = response.data;
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return <div>
+    <h1>Útilmos posts</h1>
+    {posts.length === 0 ? <p>Carregando...</p> : (posts.map((post)=>(
+      <div className="post" key={post.id}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+      </div>
+    ))
+  )}
+  </div>;
+};
+
+export default Home;
