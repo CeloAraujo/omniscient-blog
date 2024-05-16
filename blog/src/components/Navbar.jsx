4bd1efdf72b8d7accfe7a40884f011682adb86e9
celoaraujo <celoparaujo99@gmail.com>
2024-05-16 17:44:00 -0300

    update name

--- blog/src/components/Navbar.jsx
+++ blog/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
         <h2>
-        <Link to={`/`}>Blog</Link>   
+        <Link to={`/`}>Omniscient Blog</Link>   
         </h2>
         <ul>
             <li>
