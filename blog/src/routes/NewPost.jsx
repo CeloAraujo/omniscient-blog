07580a0d3d522e1aebc3ef6b31ee08a3d661177b
celoaraujo <celoparaujo99@gmail.com>
2024-05-16 18:07:05 -0300

    New Post page

--- blog/src/routes/NewPost.jsx
+++ blog/src/routes/NewPost.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import './NewPost.css'
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
-  )
-}
+    <div className="new-post">
+      <h2>Novo Post :</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            name="title"
+            placeholder="Digite o título"
+            id="title"
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Escreva aqui o conteúdo do seu Post"
+          ></textarea>
+        </div>
+        <input type="text" value="Criar Post" className='btn' />
+      </form>
+    </div>
+  );
+};
 
-export default NewPost
+export default NewPost;
