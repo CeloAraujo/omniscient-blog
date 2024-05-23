730f1f824e2d5069b4ce84288399cf4b72ce4b19
celoaraujo <celoparaujo99@gmail.com>
2024-05-23 20:52:32 -0300

    .

--- blog/src/main.jsx
+++ blog/src/main.jsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/omniscient-blog",
+        path: "/omniscient-blog/",
         element: <Home />,
       },
       {
